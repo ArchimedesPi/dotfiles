b10e64c2c06ac3514e053cb21df5cc15767d541b
Liam Marshall <archimedespi3141@gmail.com>
2015-10-13 12:26:57 -0500

    Add EAGLE to path

--- home/.zsh/path.zsh
+++ home/.zsh/path.zsh
@@ -5,7 +5,8 @@ path+=(
     $HOME/.local/bin
     /opt/android-studio/bin
     $HOME/.rvm/bin
-    $HOME/gradle/bin)
+    $HOME/gradle/bin
+    $HOME/eagle-7.4.0/bin)
 
 path+="/home/liam/perl5/bin"
 PERL5LIB="/home/liam/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
