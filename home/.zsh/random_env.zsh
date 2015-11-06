1696ad97de8a2976723345a9af09d4fa0d86b7da
Liam Marshall <archimedespi3141@gmail.com>
2015-11-06 11:13:17 -0600

    Yay, random environment variables!

--- home/.zsh/random_env.zsh
+++ home/.zsh/random_env.zsh
@@ -11,3 +11,13 @@ export ANDROID_HOME=$HOME/Android/Sdk
 
 # use homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+
+# arduino-makefile stuff
+export ARDUINO_DIR=/usr/share/arduino
+export ARDMK_DIR=/home/liam/github/sudar/Arduino-Makefile
+export AVR_TOOLS_DIR=$ARDUINO_DIR/hardware/tools/avr
+
+export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
+
+export DEBFULLNAME="Liam Marshall"
+export DEBEMAIL="shakespeares.integral@gmail.com"
