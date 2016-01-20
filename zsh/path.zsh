5d0933b8944960747f506852a55b26015dc904f6
Liam Marshall <liam@cpan.org>
2016-01-20 13:22:48 -0600

    Remove perl environment config

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -3,8 +3,3 @@ typeset -U path
 path+=(
     $HOME/bin)
 
-path+="/home/liam/perl5/bin"
-PERL5LIB="/home/liam/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/liam/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/liam/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/liam/perl5"; export PERL_MM_OPT;
