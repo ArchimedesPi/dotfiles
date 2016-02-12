55ec810a97403b20ae700b01c8a82814d4050a6b
Liam Marshall <liam@cpan.org>
2016-02-12 15:24:26 -0600

    ssh-agent with systemd and unix sockets

--- zsh/random_env.zsh
+++ zsh/random_env.zsh
@@ -5,3 +5,5 @@ export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 
 export DEBFULLNAME="Liam Marshall"
 export DEBEMAIL="shakespeares.integral@gmail.com"
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
